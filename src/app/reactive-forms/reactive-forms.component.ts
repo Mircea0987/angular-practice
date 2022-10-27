729eb378158be4331b9fc5fc0033552f719ed681
=Calugaru Mircea-Costin <=>
2022-10-27 15:10:21 +0300

    10

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,60 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder, FormArray, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  FormArray,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
-  styleUrls: ['./reactive-forms.component.css']
+  styleUrls: ['./reactive-forms.component.css'],
 })
 export class ReactiveFormsComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor(private formBuilder:FormBuilder) { }
-
-  get guest(){
-    return this.formGroup.get('guest') as FormArray
+  get guest() {
+    return this.formGroup.get('guest') as FormArray;
   }
 
-  formGroup: FormGroup
-
-  creditCardForm:FormGroup
-
-
-
+  formGroup: FormGroup;
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
-      roomId:[''],
-      guestEmail:[''],
-      checkinDate:[''],
-      checkoutDate:[''],
+      roomId: ['2'],
+      guestEmail: [
+        '',
+        {
+          validators: [
+            Validators.required,
+            Validators.email,
+            Validators.minLength(10),
+          ],
+        },
+      ],
+      checkinDate: [''],
+      checkoutDate: [''],
       address: this.formBuilder.group({
-        address1:[''],
-        country:[''],
-        zipCode:[''],
+        address1: [
+          '',
+          { validators: [Validators.minLength(5), Validators.required] },
+        ],
+        country: [''],
+        zipCode: [''],
       }),
       guest: this.formBuilder.array([
-        {guestName:[''],age:new FormControl('')}
-      ])
-    }),
-    this.formGroup = this.formBuilder.group({
-      name:('')
-    })
-    this.creditCardForm = this.formBuilder.group({
-      creditCard:(''),
-      firstName:(''),
-      lastName:(''),
-
-    })
+        { guestName: [''], age: new FormControl('') },
+      ]),
+    });
   }
-  submitForm(){
-     console.log(this.formGroup.value)
+  submitForm() {
+    console.log(this.formGroup.value);
+    this.formGroup.reset({
+      roomId: '',
+      guestEmail: '',
+      checkinDate: '',
+      checkoutDate: '',
+      address: {
+        address1: '',
+        country: '',
+        zipCode: '',
+      },
+    });
   }
 
-  addGuest(){
+  addGuest() {
     this.guest.push(
-      this.formBuilder.group({guestName:[''],age:new FormControl('')})
-    )
+      this.formBuilder.group({ guestName: [''], age: new FormControl('') })
+    );
   }
-
-
 }
